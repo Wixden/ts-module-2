8378c6d53629a06adcda63f7f1480a566508b4bc
Wixden <watchonlytutorial@gmail.com>
2023-11-06 05:28:16 +0600

    Chapter 3 Complete

--- src/2.2.ts
+++ src/2.2.ts
@@ -40,5 +40,23 @@
     roll: 10,
   };
 
+  // js --> object, array --> object, fuction --> object,
+  type Roll = number[];
+
+  interface Roll2 {
+    [index: number]: number;
+  }
+
+  const roll: Roll = [1, 2, 3];
+  const roll2: Roll2 = [1, 2, 3]; //0 1 2 3 -> number type index
+
+  type Add1 = (num1: number, num2: number) => number;
+  interface Add2 {
+    (num1: number, num2: number): number;
+  }
+
+  const add: Add1 = (num1, num2) => num1 + num2;
+  const add2: Add2 = (num1, num2) => num1 + num2;
+
   //
 }
--- src/2.3.ts
+++ src/2.3.ts
@@ -17,5 +17,31 @@
   // const boolArray: boolean[] = [true, false, false, true, false];
   const boolArray: GenericArray<boolean> = [true, false, true, false];
 
+  // Array of objects
+  // In here we are declaring the type for the object property values. Now we can't make mistakes even if we want to.
+  const user: GenericArray<{ name: string; age: number }> = [
+    {
+      name: "John",
+      age: 30,
+    },
+    {
+      name: "Jane",
+      age: 20,
+    },
+    {
+      name: "Bob",
+      age: 10,
+    },
+  ];
+
+  // Generic Tuple:
+  type GenericTuple<X, Y> = [X, Y];
+  const couple: GenericTuple<string, string> = ["John", "Jane"];
+
+  const User: GenericTuple<number, { name: string; email: string }> = [
+    123,
+    { name: "John", email: "john@example.com" },
+  ];
+
   //
 }
