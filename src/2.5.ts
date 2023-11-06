dbff9afd12fcc7dc2a4fcc1489b58e8664605f71
Wixden <watchonlytutorial@gmail.com>
2023-11-06 06:22:56 +0600

    Tuple with generic types

--- src/2.5.ts
+++ src/2.5.ts
@@ -25,6 +25,28 @@
   });
 
   // tuple with generic function
+  const createArrayWithTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
+    return [param1, param2];
+  };
+
+  const result4 = createArrayWithTuple<string, number>("Bangladesh", 2023);
+
+  interface User2 {
+    id: number;
+    name: string;
+    email: string;
+    phone: number;
+  }
+
+  const result5 = createArrayWithTuple<User2, string>(
+    {
+      id: 1,
+      name: "Akhtar",
+      email: "akhtar@gmail.com",
+      phone: 987654321,
+    },
+    "Bangladesh"
+  );
 
   //
 }
