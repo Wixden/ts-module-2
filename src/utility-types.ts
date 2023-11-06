c47437d067b905458eb450b8522f237abd50d31e
Wixden <watchonlytutorial@gmail.com>
2023-11-07 05:04:06 +0600

    learned new types [Pick, Omit, Required, Partial]

--- src/utility-types.ts
+++ src/utility-types.ts
@@ -2,18 +2,24 @@
   // ==============================================================
 
   // utility types
-  interface Person {
+  type Person = {
     name: string;
     age: number;
     email: string;
     phone?: string;
-  }
+  };
 
   // creating a new type from an existing type
   type NameAndAge = Pick<Person, "name" | "email">;
 
   // Omit;
   type ContactInfo = Omit<Person, "name" | "age">;
+
+  // required type (it will make a new type with the existing type with all the filled required fields)
+  type RequiredPerson = Required<Person>;
+
+  // Partial type (it will make a new type with the existing type with all the filled partial fields)
+  type PartialPerson = Partial<Person>;
 
   // ==============================================================
 }
