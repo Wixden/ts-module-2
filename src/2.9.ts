648089c7a6084dbf05bc6d15441e2da4f629781d
Wixden <watchonlytutorial@gmail.com>
2023-11-07 04:08:08 +0600

    dynamic conditional type

--- src/2.9.ts
+++ src/2.9.ts
@@ -8,7 +8,26 @@
 
   type x = a1 extends null ? true : false; // conditional type;
 
-  type y = a1 extends null ? true : b1 extends undefined ? undefined : any;
+  type y = a1 extends null ? true : a1 extends undefined ? undefined : any;
+
+  interface Sheikh {
+    bike: string;
+    car: string;
+    ship: string;
+    tractor: string;
+    boat: string;
+  }
+
+  // check if it has a bike | car | ship manually (not the correct way)
+  type CheckVehicle<T> = T extends "bike" | "car" | "ship" ? true : false;
+
+  // check the key dynamically
+  type CheckVehicle2<T> = T extends keyof Sheikh ? true : false;
+
+  type HasBike = CheckVehicle<"bike">; //true
+  type HasTractor = CheckVehicle<"tractor">; //false
+
+  type HasBoat = CheckVehicle2<"boat">; //true
 
   // ==============================================================
 }
