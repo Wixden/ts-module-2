1705a4dbf70750bc2672f66fac88c30d68b7cc68
Wixden <watchonlytutorial@gmail.com>
2023-11-09 05:04:20 +0600

    updated access modifiers

--- src/mapped-types.ts
+++ src/mapped-types.ts
@@ -9,7 +9,7 @@
 
   // console.log(arrOfString);
 
-  type BoxSize = {
+  type BoxSizeNumber = {
     width: number;
     height: number;
     depth: number;
@@ -24,11 +24,11 @@
   // }
 
   // now in here converting the same value type to different type using mapped types
-  type BoxSizeString = { [key in keyof BoxSize]: string };
-  type BoxSizeBoolean = { [key in keyof BoxSize]: boolean };
+  type BoxSizeString = { [key in keyof BoxSizeNumber]: string };
+  type BoxSizeBoolean = { [key in keyof BoxSizeNumber]: boolean };
 
   // now check using all declared types
-  const boxSize1: BoxSize = {
+  const boxSize1: BoxSizeNumber = {
     width: 10,
     height: 20,
     depth: 30,
@@ -55,9 +55,9 @@
   console.log({ boxSize1 }, { boxSize2 }, { boxSize3 });
 
   // Look up type
-  type Height = BoxSize["height"]; // Look up type
-  type Width = BoxSize["width"]; // Look up type
-  type Depth = BoxSize["depth"]; // Look up type
+  type Height = BoxSizeNumber["height"]; // Look up type
+  type Width = BoxSizeNumber["width"]; // Look up type
+  type Depth = BoxSizeNumber["depth"]; // Look up type
 
   // generic mapped types
   type BoxSizeString2<T> = {
